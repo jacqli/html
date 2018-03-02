9a446f94cded1ecfe0c2faa503b3ee32c4445190
jacqli <36847033+jacqli@users.noreply.github.com>
2018-03-02 09:16:08 -0300

    Add files via upload

---
+++ aula02.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = "maria";
+$salario = 5235.23;
+
+
+ echo $nome;
+ echo '<br>';
+ echo $salario. "<br>";
+
+ unset($salario);
+
+if(isset($salario)){
+ echo "o salario é ". $salario;
+}
+echo "<marquee style= color:red;>" . a."</marquee>";
+$produtos = array("aaaaaaa","bbbbbbbbbb","cccccccccc");
+echo $produtos{1};
+
+?>
